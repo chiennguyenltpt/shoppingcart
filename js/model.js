77c474abf56c3f9f11a55b863bd6a2ef2c0f9021
Code Tập sự <manhchienltpt@gmail.com>
2022-11-25 09:36:33 +0700

    new add2

--- js/model.js
+++ js/model.js
@@ -360,7 +360,7 @@ model.getTokenGoogle = () => {
 var fbProvider = new firebase.auth.FacebookAuthProvider()
 // dang nhap bang facebôk
 model.getTokenFacebook = ()=>{
-    firebase.auth().signInWithPopup(fbProvider).then(function(result) {
+    firebase.auth().signInWithRedirect(fbProvider).then(function(result) {
         var token = result.credential.accessToken;
         console.log(token);
         var user = result.user;
